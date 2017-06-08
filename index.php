ce70822c3c98cd30c88d97bfa72d253738c27ed5
Mahady Hasan <shoyshab@gmail.com>
2017-06-09 04:11:52 +0600

    ok

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php include 'config/config.php';?>
+<?php include 'lib/Database.php';?>
+<?php include 'inc/header.php';?>
+<?php include 'inc/slider.php';?>
+<?php include 'helpers/format.php';?>
+
+
+<?php
+	$db = new Database();
+	$fm = new format();
+?>
+	
+
+<div class="contentsection contemplete clear">
+	<div class="maincontent clear">
+	<!--Pagination-->
+	<?php
+	$per_page = 3;
+	if(isset($_GET["page"])) {
+		$page = $_GET["page"];
+	} else {
+		$page=1;
+	}
+	$start_form = ($page-1) * $per_page;
+	?>
+	<?php 
+		$query = "select * from tbl_post limit $start_form,$per_page";
+		$post = $db->select($query);
+		if ($post) {
+			while ($result = $post -> fetch_assoc()) {
+	 ?>
+<div class="samepost clear">
+	<h2><a href="post.php?id=<?php echo $result ['id']; ?>">"<?php echo $result ['title']; ?>"</a></h2>
+	<h4><?php echo $fm->formatDate($result ['date']); ?><a href="#"><?php echo $result ['author']; ?></a></h4>
+	 <a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+	<?php echo $fm->textShorten($result ['body']); ?>
+	<div class="readmore clear">
+		<a href="post.php?id=<?php echo $result ['id']; ?>">Read More</a>
+	</div>
+</div>
+<?php } ?> <!--end of while loop-->
+
+<!--Pagination-->
+<?php 
+$query = "select * from tbl_post";
+$result = $db->select($query);
+$total_row = mysqli_num_rows($result);
+$total_pages = ceil($total_row/$per_page);
+
+
+echo "<span class = 'pagination'><a href = 'index.php?page=1'>".'First Page'."</a>"; 
+for ($i=1; $i <= $total_pages; $i++) { 
+	echo "<a href = 'index.php?page=".$i."'> | ".$i." | </a>";
+}
+ echo "<a href = 'index.php?page=$total_pages'>".'Last Page'."</a></span>"; ?>
+<!--Pagination-->
+
+<?php } else {header("Location: 404.php");}?> <!-- end of if condition -->
+		</div>
+
+<?php include 'inc/sidebar.php';?>
+
+
+<?php include 'inc/footer.php';?>
+	
